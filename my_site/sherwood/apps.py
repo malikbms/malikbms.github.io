0da37357b2097130dc4040f3619dc4b65dc48070
malikbms <47319809+malikbms@users.noreply.github.com>
2019-12-18 17:15:07 +0100

    Add files via upload

---
+++ my_site/sherwood/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SherwoodConfig(AppConfig):
+    name = 'sherwood'
---
+++ my_site/sherwood/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.phone_number = form.cleaned_data.get('phone_number')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
